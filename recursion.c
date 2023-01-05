4119e8cf40728a589170fb5f65a0913dec70ea94
slayer889 <95080147+slayer889@users.noreply.github.com>
2023-01-05 18:25:41 +0530

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//function to calculate square of a number
+int calcSquare(int n);
+int main() {
+int n;
+printf("enter n : ");
+scanf("%d", &n);
+printf("square is : %d", calcSquare(n));
+return 0;
+}
+int calcSquare(int n) {
+return n * n;
+}
